48faadccd8ee5a5b0949ecdd05c34bc796af017a
aroenvr <aroenvr@gmail.com>
2022-06-03 19:40:23 +0200

    first commit

---
+++ src/main/java/com/breaker/chesserz/ChesserzApplication.java
@@ -0,0 +1,13 @@
+package com.breaker.chesserz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChesserzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChesserzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/breaker/chesserz/ChesserzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breaker.chesserz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChesserzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
